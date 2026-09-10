683104e61ff4133911e4b9765a7deba83434c2d4
Malico <malico@desgn.space>
2026-09-10 10:03:15 +0100

    chore(cask): update Oga to 0.1.0
    Update the cask checksum for the 0.1.0 release.

--- Casks/oga.rb
+++ Casks/oga.rb
@@ -1,6 +1,6 @@
 cask "oga" do
-  version "0.0.11"
-  sha256 "7365daa8abcbbf7e3710255c7fe4dac6ea905a82bdde4c3c2a3d878bcdc1c192"
+  version "0.1.0"
+  sha256 "e141749562e8805401c54a02803dda14c38fa4843f55bb521720a2f41217f1ad"
 
   url "https://downloads.desgn.space/oga/Oga-#{version}.zip"
   name "Oga"
